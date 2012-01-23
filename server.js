b8aeccd324f3eae2f59819507a7383e9073c644b
Inanc Sevinc <inancsevinc@gmail.com>
2012-01-24 00:11:28 +0200

    port

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Nodejs Labs\n');
-}).listen(80, "localhost");
+}).listen(process.env.PORT, "0.0.0.0");
 console.log('Server running at http://localhost:80/');
