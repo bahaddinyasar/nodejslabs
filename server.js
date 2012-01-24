0831a2e4cbe9d3641d51bf9a4eee04afb862bcfc
Inanc Sevinc <inancsevinc@gmail.com>
2012-01-24 22:54:46 +0000

    test

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-var express = require('express');
+var express = require('express'),
+    app = express.createServer();
 
-var app = express.createServer();
+app.register('.html', require('jade'));
+app.set("view options", {
+    layout: false
+});
 
 app.configure(function() {
     app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	app.use(express.logger());
 	app.use(app.router);
-	app.use(express.static(__dirname + '/public')); // url: http://..../createUser.html
-	// app.use('/public',express.static(__dirname + '/public')); // url:  http://..../public/createUser.html
+	app.use(express.static(__dirname + '/public'));
 });
 
-app.get('/', function(req, res) {
-    res.send('Hello Nodejslabs\n');
-	//res.render('index.html');
-});
 
 app.listen(process.env.PORT);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
