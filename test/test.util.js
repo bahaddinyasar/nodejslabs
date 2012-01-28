33d064c00609204a3713a9ea1c8555547aa503c2
Inanc Sevinc <inancsevinc@gmail.com>
2012-01-28 13:51:15 +0000

    test

--- test/test.util.js
+++ test/test.util.js
@@ -4,14 +4,9 @@ var assert = require('should'),
 describe('util.getDirectoryContents()', function() {
     it('should return nonzero items', function(done) {
         util.getDirectoryContents('.', '', function(files) {
-            files.length.should.be.above(200);
-            (4).should.equal(5);
+            files.length.should.be.above(0);
             done();
         });
     });
-    it('should fail', function() {
-            (4).should.equal(5);
-            done();
-       
-    });
+   
 });
