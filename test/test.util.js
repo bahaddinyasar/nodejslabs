7f4acbf6933f486d7adce715601eadba4ed6c1ad
Inanc Sevinc <inancsevinc@gmail.com>
2012-01-28 14:19:01 +0000

    tdd

--- test/test.util.js
+++ test/test.util.js
@@ -1,17 +1,17 @@
 var assert = require('should'),
     util = require('../util.js');
 
-describe('util.getDirectoryContents()', function() {
-    it('should return nonzero items', function(done) {
+suite('util.getDirectoryContents()', function() {
+    test('should return nonzero items', function(done) {
         util.getDirectoryContents('.', '', function(files) {
             files.length.should.be.above(0);
             done();
         });
     });
-    it('should return zero items for dummy extension', function(done) {
+    test('should return zero items for dummy extension', function(done) {
         util.getDirectoryContents('.', 'asdfasd', function(files) {
             files.length.should.equal(0);
-            done();
+            done(); 
         });
     });
    
