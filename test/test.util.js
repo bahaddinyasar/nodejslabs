c4a1f252669baf3ba597dc9b9315c05eb1ef3009
Inanc Sevinc <inancsevinc@gmail.com>
2012-01-28 13:52:55 +0000

    travis

--- test/test.util.js
+++ test/test.util.js
@@ -8,5 +8,11 @@ describe('util.getDirectoryContents()', function() {
             done();
         });
     });
+    it('should return zero items for dummy extension', function(done) {
+        util.getDirectoryContents('.', 'asdfasd', function(files) {
+            files.length.should.be.above(0);
+            done();
+        });
+    });
    
 });
