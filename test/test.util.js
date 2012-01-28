7eec5158d8ed2e0cc353ef6e06ec83bfaeabdb58
Inanc Sevinc <inancsevinc@gmail.com>
2012-01-28 13:42:46 +0000

    mocha

--- test/test.util.js
+++ test/test.util.js
@@ -2,7 +2,7 @@ var assert = require('should'),
     util = require('../util.js');
 
 describe('util.getDirectoryContents()', function() {
-    it('should return nonzero items', function() {
+    it('should return nonzero items', function(done) {
         util.getDirectoryContents('..', '', function(files) {
             files.length.should.be.above(200);
             (4).should.equal(5);
