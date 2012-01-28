f8b34639343ba075a1244eae8a5c46ab727f1f1f
Inanc Sevinc <inancsevinc@gmail.com>
2012-01-28 13:27:46 +0000

    test

--- test/test.util.js
+++ test/test.util.js
@@ -4,7 +4,7 @@ var assert = require('should'),
 describe('util.getDirectoryContents()', function() {
     it('should return nonzero items', function() {
         util.getDirectoryContents('..', '', function(files) {
-            files.length.should.be.above(0);
+            files.length.should.be.above(20);
             console.log('length: '+files.length);
             done();
         });
