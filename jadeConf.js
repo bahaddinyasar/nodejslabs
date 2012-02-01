165fbec1086c3247eba5d7d0ed46f10321db3ebd
Inanc Sevinc <inancsevinc@gmail.com>
2012-02-01 23:03:55 +0000

    add session data

--- jadeConf.js
+++ jadeConf.js
@@ -1,7 +1,10 @@
 
 module.exports.configurePaths = function (app) {
   
-  app.get('/session_vars', function (req, res) {
+  app.get('/session_vars/:op?/:key?/:value?', function (req, res) {
+    if(req.params.op) {
+      req.session[req.params.key] = req.params.value;
+    }
     res.render('session_vars',{'session':req.session});
   });
   
