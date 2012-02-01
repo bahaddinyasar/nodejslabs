eb74847ec37c6359964338f07d6b9f1e96b030e6
Inanc Sevinc <inancsevinc@gmail.com>
2012-02-01 23:13:02 +0000

    jade get

--- jadeConf.js
+++ jadeConf.js
@@ -2,8 +2,8 @@
 module.exports.configurePaths = function (app) {
   
   app.get('/session_vars', function (req, res) {
-    if(req.body.op) {
-      req.session[req.body.key] = req.body.value;
+    if(req.query.op) {
+      req.session[req.query.key] = req.query.value;
     }
     res.render('session_vars',{'session':req.session});
   });
