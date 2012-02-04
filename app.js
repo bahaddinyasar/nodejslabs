5948f8ab34efc91be92afc23230c55b468744fe9
Inanc Sevinc <inancsevinc@gmail.com>
2012-02-04 12:04:19 +0000

    app.js

---
+++ app.js
@@ -0,0 +1,57 @@
+var express = require('express'),
+    app = express.createServer(),
+    util = require('./util.js'),
+    dateformat = require('dateformat'),
+    io = require('socket.io').listen(app),
+    chat = require('./chat.js'),
+    everyauth = require('everyauth'),
+    jadeConfig = require('./jadeConf.js');
+ 
+app.register('.html', require('jade'));
+
+app.set("view options", {
+    layout: false
+});
+
+app.configure(function() {
+    app.use(express.bodyParser());
+	app.use(express.methodOverride());
+    app.use(express.cookieParser());
+    app.use(express.session({ secret: "nodejslabssecret" }));
+   // app.use(everyauth.middleware());
+    app.set('view engine', 'jade');
+	app.use(express.logger());
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'));
+});
+
+jadeConfig.configurePaths(app);
+
+app.get('/code/:fileName?', function(req, res) {
+	var filepath = req.params.fileName ? __dirname + '/' + req.params.fileName : __filename;
+	util.getFileContents(filepath, function(data) {
+		res.send(data);
+	});
+});
+
+app.get('/files/:fileExtension?', function(req, res) {
+	util.getDirectoryContents(__dirname,req.params.fileExtension, function(data) {
+		res.json(data);
+	});
+});
+
+app.get('/ping', function(req, res) {
+    res.send('pong');
+});
+
+
+//io.set('transports', ['websocket', 'jsonp-polling']);
+io.set('transports', ['xhr-polling', 'jsonp-polling']);
+io.set('log level', 4);
+chat.setIoObject(io);
+io.sockets.on('connection', chat.connectionHandler);
+
+//everyauth.helpExpress(app);
+
+app.listen(process.env.PORT);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
