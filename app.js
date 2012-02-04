defdd09f8e3e29fb085de8a9a9c7b647aadb58d3
Inanc Sevinc <inancsevinc@gmail.com>
2012-02-04 13:35:19 +0000

    refactoring

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
-var express = require('express'),
-    app = express.createServer(),
-    io = require('socket.io').listen(app),
-    chat = require('./chat.js'),
-    fs = require('fs');
+var app = require('express').createServer(),
+    io = require('socket.io').listen(app);
 
 // to make util global, omit var. 
 util = require('./util.js');
  
 require('./configuration.js').boot(app); 
 util.requireCodeForApp(__dirname+'/routes',app); 
- 
 
-//io.set('transports', ['websocket', 'jsonp-polling']);
-io.set('transports', ['xhr-polling', 'jsonp-polling']);
-io.set('log level', 4);
-chat.setIoObject(io);
-io.sockets.on('connection', chat.connectionHandler);
-
+require('./chat.js')(io);
 
 app.listen(process.env.PORT);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- chat.js
+++ chat.js
@@ -1,47 +1,51 @@
- var dateformat = require('dateformat');
-// usernames which are currently connected to the chat
-var usernames = {};
+var dateformat = require('dateformat');
 
-var ioObject;
+module.exports = function (ioObject) {
 
-module.exports.setIoObject = function(theIoObject){
-  ioObject = theIoObject;
-};
-
-module.exports.connectionHandler = function (socket) {
-  // when the client emits 'sendchat', this listens and executes
-  socket.on('sendchat', function (data) {
-    // we tell the client to execute 'updatechat' with 2 parameters
-
-    ioObject.sockets.emit('updatechat', {
-        'msg':data,
-        'user':socket.username,
-        'time': dateformat("hh:MM:ss")
-        } );
-  });
-
-  // when the client emits 'adduser', this listens and executes
-  socket.on('adduser', function(username){
-    // we store the username in the socket session for this client
-    socket.username = username;
-    // add the client's username to the global list
-    usernames[username] = username;
-    // echo to client they've connected
+    // usernames which are currently connected to the chat
+    var usernames = {};
     
-    socket.emit('updatechat', {'msg':'you have connected','user':'SERVER','time':dateformat("hh:MM:ss")});
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('updatechat', {'msg':username + ' has connected','user':'SERVER','time':dateformat("hh:MM:ss")});
-    // update the list of users in chat, client-side
-    ioObject.sockets.emit('updateusers', usernames);
-  });
+    ioObject.set('transports', ['xhr-polling', 'jsonp-polling']);
+    ioObject.set('log level', 4);
+    
+    var connectionHandler = function (socket) {
+      // when the client emits 'sendchat', this listens and executes
+      socket.on('sendchat', function (data) {
+        // we tell the client to execute 'updatechat' with 2 parameters
+    
+        ioObject.sockets.emit('updatechat', {
+            'msg':data,
+            'user':socket.username,
+            'time': dateformat("hh:MM:ss")
+            } );
+      });
+    
+      // when the client emits 'adduser', this listens and executes
+      socket.on('adduser', function(username){
+        // we store the username in the socket session for this client
+        socket.username = username;
+        // add the client's username to the global list
+        usernames[username] = username;
+        // echo to client they've connected
+        
+        socket.emit('updatechat', {'msg':'you have connected','user':'SERVER','time':dateformat("hh:MM:ss")});
+        // echo globally (all clients) that a person has connected
+        socket.broadcast.emit('updatechat', {'msg':username + ' has connected','user':'SERVER','time':dateformat("hh:MM:ss")});
+        // update the list of users in chat, client-side
+        ioObject.sockets.emit('updateusers', usernames);
+      });
+    
+      // when the user disconnects.. perform this
+      socket.on('disconnect', function(){
+        // remove the username from global usernames list
+        delete usernames[socket.username];
+        // update list of users in chat, client-side
+        ioObject.sockets.emit('updateusers', usernames);
+        // echo globally that this client has left
+        socket.broadcast.emit('updatechat', {'msg':socket.username + ' has disconnected','user':'SERVER','time':dateformat("hh:MM:ss")});
+      });
+    };
+    
+    ioObject.sockets.on('connection', connectionHandler);
 
-  // when the user disconnects.. perform this
-  socket.on('disconnect', function(){
-    // remove the username from global usernames list
-    delete usernames[socket.username];
-    // update list of users in chat, client-side
-    ioObject.sockets.emit('updateusers', usernames);
-    // echo globally that this client has left
-    socket.broadcast.emit('updatechat', {'msg':socket.username + ' has disconnected','user':'SERVER','time':dateformat("hh:MM:ss")});
-  });
 };
