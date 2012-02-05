73bb24af095c8391eaf5199358457343a65b2038
Inanc Sevinc <inancsevinc@gmail.com>
2012-02-05 16:21:01 +0000

    everyauth setup

--- configuration.js
+++ configuration.js
@@ -2,6 +2,27 @@ var express = require('express'),
     everyauth = require('everyauth');
 
 module.exports.boot = function (app) {
+    
+    var usersById = {};
+    var nextUserId = 0;
+    var usersByTwitId = {};
+    function addUser (source, sourceUser) {
+      var user;
+       // non-password-based
+        user = usersById[++nextUserId] = {id: nextUserId};
+        user[source] = sourceUser;
+      
+      return user;
+    }
+
+    everyauth.debug = true;
+    everyauth.twitter
+    .consumerKey('RrOhI3kPbI3fsaIjYklKkg')
+    .consumerSecret('l8oH2Ela9WV0UFfdGrF62kuGXU1qKhZjO3dvIdJ7h0')
+    .findOrCreateUser( function (sess, accessToken, accessSecret, twitUser) {
+      return usersByTwitId[twitUser.id] || (usersByTwitId[twitUser.id] = addUser('twitter', twitUser));
+    })
+    .redirectPath('/secure');
 
     app.register('.html', require('jade'));
     
@@ -10,6 +31,7 @@ module.exports.boot = function (app) {
     });
     
     app.configure(function() {
+        app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
         app.use(express.bodyParser());
         app.use(express.methodOverride());
         app.use(express.cookieParser());
