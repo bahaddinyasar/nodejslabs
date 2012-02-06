246317465f461856ff551f3c90bd6a40a7b83c9b
Inanc Sevinc <inancsevinc@gmail.com>
2012-02-06 22:55:23 +0000

    authorization

--- authorization.js
+++ authorization.js
@@ -1,6 +1,27 @@
 var everyauth = require('everyauth');
 
-module.exports = function(app) {
+module.exports = function(app,everyauth) {
+    
+     var usersById = {};
+    var nextUserId = 0;
+    var usersByTwitId = {};
+    function addUser (source, sourceUser) {
+      var user;
+       // non-password-based
+        user = usersById[++nextUserId] = {id: nextUserId};
+        user[source] = sourceUser;
+      
+      return user;
+    }
+
+    everyauth.debug = true;
+    everyauth.twitter
+    .consumerKey('RrOhI3kPbI3fsaIjYklKkg')
+    .consumerSecret('l8oH2Ela9WV0UFfdGrF62kuGXU1qKhZjO3dvIdJ7h0')
+    .findOrCreateUser( function (sess, accessToken, accessSecret, twitUser) {
+      return usersByTwitId[twitUser.id] || (usersByTwitId[twitUser.id] = addUser('twitter', twitUser));
+    })
+    .redirectPath('/login');
   
     app.all('/secure*', function(req, res, next) {
         console.dir(req.session);
