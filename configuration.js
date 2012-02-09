1df1e9855375f35648e7ffe43c5152654f2b6235
Mehmet Bahaddin Yaşar <bahaddinyasar@gmail.com>
2012-02-09 20:27:08 +0000

    "mongohq user i duzeltildi"

--- configuration.js
+++ configuration.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 // to make util global, omit var.
-dbconnection = require('mongoskin').db('mongodb://nodejslab:deneme1234@staff.mongohq.com:10017/nodejslabs');
+dbconnection = require('mongoskin').db('mongodb://nodejslabs:deneme1234@staff.mongohq.com:10017/nodejslabs');
 
 everyauth = require('everyauth');
 
