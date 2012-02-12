2ce553f36e5fb8e8f239cff1d6c112b2d9a934d4
Mehmet Bahaddin Yaşar <bahaddinyasar@gmail.com>
2012-02-12 20:53:54 +0000

    "controller dan kullanim icin degisiklik yapildi"

--- routes/api_users.js
+++ routes/api_users.js
@@ -1,58 +1,101 @@
-module.exports = function(app) {
-    app.post('/api/users',      this._create);
-    app.get('/api/users',       this._retrieveAll);
-    app.get('/api/users/:id',   this._retrieve);
-    app.del('/api/users/:id',   this._delete);
-    app.put('/api/users/:id',   this._update);  
-};
+var users = dbconnection.collection('users');
 
-module.exports._retrieveAll = function(req, res) {
-    var users = dbconnection.collection('users');
-    users.find().toArray(function(err, items) {
-        if (err) throw err;
-        res.json(items);
-	});
-};
+module.exports = {
+    mapping: {
+		"index" : {
+			"url":"/users", 
+			"method":"get", 
+			"description":"retrieve all users",
+			"auth":false
+		},
+		"create" : {
+			"url":"/users", 
+			"method":"put",
+			"description":"create a new user",
+			"auth":false
+		},
+		"read" : {
+			"url":"/users/:id", 
+			"method":"get",
+			"description":"get a single user by id",
+			"auth":false
+		},
+		"update" : {
+			"url":"/users/:id", 
+			"method":"post",
+			"description":"update a given user",
+			"auth":false
+		},
+		"delete" : {
+			"url":"/users/:id", 
+			"method":"delete",
+			"description":"delete your own user, attention: cant be undone",
+			"auth":true
+		}
+	},
 
-module.exports._retrieve = function(req, res) {
-    var users = dbconnection.collection('users');
-    users.findById(req.params.id, function(err,user) {
-        if (err) throw err;
-        res.json(user);
-    });
-};
+    // GET /users
+	index: function(req, res) {
+        users.find().toArray(function(err, items) {
+            if (err) throw err;
+            res.json(items);
+        });
+	}, 
 
-module.exports._create = function(req, res) {
-    var users = dbconnection.collection('users');
-    users.insert({
-        name: req.body.name,
-        surname: req.body.surname,
-        age: req.body.age,
-        email: req.body.email,
-        gender: req.body.gender 
-	}, function() {
-		 res.json({"success": "true"});
-	});
-};
-
-module.exports._delete = function(req, res) {
-    var users = dbconnection.collection('users');
-    users.removeById(req.params.id, function(err) {
-        if (err) throw err;
-        res.json({"success": "true"});
-    });
-};
-
-module.exports._update = function(req, res) {
-    var users = dbconnection.collection('users');
-    users.updateById(req.body.id, {
-        name: req.body.name,
-		surname: req.body.surname,
-		age: req.body.age,
-        email: req.body.email,
-        gender: req.body.gender 
-	}, function(err) {
-		 if (err) throw err;
-        res.json({"success": "true"});
-	});
-};
+    // GET /users/:id
+	read: function(req, res) {
+        users.findById(req.params.id, function(err,user) {
+            if (err){
+                res.json({"success": "false",   "errorMsg"  : err.message});
+			} else {
+                res.json({"success": "true",    "user"      : user});
+			}
+        });
+	}, 
+    
+    // POST /users
+	update: function(req, res) {        
+        users.updateById(req.body.id, {
+            name: req.body.name,
+        	surname: req.body.surname,
+    		age: req.body.age,
+            email: req.body.email,
+            gender: req.body.gender 
+    	}, function(err) {
+    		if (err)  {
+    			res.json({"success": "false", "errorMsg" : err.message});
+			} else {
+                res.json({"success": "true"});
+			}
+    	});
+	}, 
+    
+    // PUT /users
+	create: function(req, res) {
+        users.insert({
+            name: req.body.name,
+            surname: req.body.surname,
+            age: req.body.age,
+            email: req.body.email,
+            gender: req.body.gender 
+        }, function(err) {
+        	if (err)  {
+    			res.json({"success": "false", "errorMsg" : err.message});
+			} else {
+                res.json({"success": "true"});
+			}
+    	});
+	}, 
+    
+    // DELETE /users/:id
+	delete: function(req, res) {
+	    var users = dbconnection.collection('users');
+        users.removeById(req.params.id, function(err) {
+            if (err)  {
+    			res.json({"success": "false", "errorMsg" : err.message});
+			} else {
+                res.json({"success": "true"});
+			}
+    	});
+	}
+}
