29128df5baa4d14d5ef42bab338984bdb18fb8a8
Inanc Sevinc <inancsevinc@gmail.com>
2012-02-12 21:20:07 +0000

    'refactoring'

--- routes/code.js
+++ routes/code.js
@@ -1,16 +1,29 @@
-module.exports = function(app) {
-    
-    app.get('/code/:fileName?', function(req, res) {
+module.exports = {
+    mapping: {
+            "code_filename" : {
+			"url":"/code/:fileName?", 
+			"method":"get", 
+			"description":"retrieve source for given file",
+			"auth":false
+		},
+		    "files_fileextension" : {
+			"url":"/files/:fileExtension?", 
+			"method":"get",
+			"description":"retrieves files, filtering for given extension",
+			"auth":false
+		}
+    },
+    code_filename : function(req, res) {
         var filepath = req.params.fileName ? __dirname + '/' + req.params.fileName : __filename;
-    	util.getFileContents(filepath, function(data) {
+        util.getFileContents(filepath, function(data) {
     		res.send(data);
     	});
-    });
-    
-    app.get('/files/:fileExtension?', function(req, res) {
-    	util.getDirectoryContents(__dirname,req.params.fileExtension, function(data) {
+    },
+    files_fileextension :  function(req, res) {
+        util.getDirectoryContents(__dirname,req.params.fileExtension, function(data) {
     		res.json(data);
     	});
-    });
-
+    }
 }
+    
+    
