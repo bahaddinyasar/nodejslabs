b55c15545b5690a704f92e0d5b1bbdcd86e29657
Inanc Sevinc <inancsevinc@gmail.com>
2012-02-12 21:18:18 +0000

    'adding requireRoutes and bootController'

--- util.js
+++ util.js
@@ -29,4 +29,61 @@ module.exports.requireCodeForApp = function(dirpath, app) {
         });
     }
 };
+var mappingString = "";
+
+module.exports.requireRoutes = function(app) {
+    var files = fs.readdirSync(__dirname+'/routes');
+    if(files) {
+        files.forEach(function(file){
+            if( (/\.js$/).test(file) ) 
+                bootController(app,file);
+        });
+    }
+    app.get("/show_available_interfaces", function(req, res){
+            res.send(mappingString);
+        });
+};
+
+function add_to_mapping_string(method, url, description, auth) {
+    mappingString += "<div style='width: 400px; height: 20px; padding: 5px; background-color: #ccc; color: #000; font-family: Arial, Verdana, sans-serif'><b>" + method + " " + url + "</b></div><div style='width: 400px; padding: 5px; background-color: #eee; color: #000; font-family: Arial, Verdana, sans-serif'>" + description + "<br /><b>Auth:</b> " + auth + "</div><br />";
+}
+
+function bootController(app, file) {
+  var name = file.replace('.js', '');
+  var actions = require(__dirname+'/routes/' + name);
+
+  var mapping = actions["mapping"];
+
+  Object.keys(actions).map(function(action){
+	  var fn = actions[action];
+
+	  if(typeof(fn) === "function") {
+		  if(a = mapping[action]) {
+            add_to_mapping_string(a.method, a.url, a.description, a.auth);
+		  	switch(a.method) {
+		  		case 'get':
+		  					app.get(a.url, fn);
+		  					console.log("initialized get " + a.url);
+		  					break;
+		  		case 'post':
+		  					app.post(a.url, fn);
+		  					console.log("initialized post " + a.url);
+		  					break;
+		  		case 'put':
+		  					app.put(a.url, fn);
+		  					console.log("initialized put " + a.url);
+		  					break;
+		  		case 'delete':
+		  					app.del(a.url, fn);
+		  					console.log("initialized delete " + a.url);
+		  					break;
+		  	}
+		  } else {
+		  	console.log("WARNING: no mapping for " + action + " defined");
+		  }
+	}
+ });
+}
+
+
   
