8059da0af80f4a49dec17c72152c0f70f5b6e6d0
Inanc Sevinc <inancsevinc@gmail.com>
2012-02-12 12:09:04 +0000

    'api refactoring'

---
+++ routes/api_users.js
@@ -0,0 +1,47 @@
+module.exports = function(app) {
+    
+    var users = dbconnection.collection('users');
+    
+    app.get('/users', function(req, res) {
+        users.find().toArray(function(err, items) {
+    	    if (err) throw err;
+		    res.json(items);
+    	});
+    });
+    app.post('/users', function(req, res) {
+        users.insert({
+            name: req.body.name,
+        	surname: req.body.surname,
+    		age: req.body.age,
+            email: req.body.email,
+            gender: req.body.gender 
+    	}, function() {
+    		 res.json({"success": "true"});
+    	});
+    });
+    app.get('/users/:id', function(req, res) {
+        users.findById(req.params.id, function(err,user) {
+            if (err) throw err;
+    	    res.json(user);
+        });
+    });
+    app.del('/users/:id', function(req, res) {
+        users.removeById(req.params.id, function(err) {
+            if (err) throw err;
+            res.json({"success": "true"});
+    	});
+    });
+    app.put('/users/:id', function(req, res) {
+        users.updateById(req.body.id, {
+        	name: req.body.name,
+    		surname: req.body.surname,
+    		age: req.body.age,
+            email: req.body.email,
+            gender: req.body.gender 
+    	}, function(err) {
+    		 if (err) throw err;
+            res.json({"success": "true"});
+    	});
+    });
+     
+};
